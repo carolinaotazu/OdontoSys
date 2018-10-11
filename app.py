84eedce04b4330ceb3eec1deb51d01806db83007
Carolina Maldonado <carolinam621@gmail.com>
2018-10-11 20:50:47 -0300

    Template listo

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    if request.method=='POST':
+        usuario = request.form['usuario']
+        contra = request.form['contra']
+        return 'Hola ' + usuario + ', tu contraseña es: ' + contra
 
 if __name__ == '__main__':
     app.run()
